5addf94b5428a1f515c8e9472b7ffae4bb2e8778
Краев Михаил Игоревич <MKraev@inno.tech>
2022-07-31 23:31:32 +0300

    refactor

--- src/main/kotlin/com/gelugu/home/Application.kt
+++ src/main/kotlin/com/gelugu/home/Application.kt
@@ -7,7 +7,6 @@ import com.gelugu.home.plugins.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
-import org.jetbrains.exposed.sql.Database
 
 fun main() {
     checkEnvironment()
