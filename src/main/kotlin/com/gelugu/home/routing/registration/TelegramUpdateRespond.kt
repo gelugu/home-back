2adee83da1bf8db816ca8eba7a7831ceb4e54f77
Краев Михаил Игоревич <MKraev@inno.tech>
2022-08-11 21:30:55 +0300

    fix

--- src/main/kotlin/com/gelugu/home/routing/registration/TelegramUpdateRespond.kt
+++ src/main/kotlin/com/gelugu/home/routing/registration/TelegramUpdateRespond.kt
@@ -52,34 +52,3 @@ data class TelegramUpdateRespondMessageEntity(
   val length: Int,
   val type: String
 )
-
-//{
-//  "ok":true,
-//  "result": [
-//    {
-//      "update_id":119782897,
-//      "message": {
-//        "message_id":448,
-//        "from": {
-//          "id":218596157,
-//          "is_bot":false,
-//          "first_name":"Mikhail",
-//          "last_name":"Kraev",
-//          "username":"gelugu",
-//          "language_code":"en"
-//        },
-//        "chat": {
-//          "id":218596157,
-//          "first_name":"Mikhail",
-//          "last_name":"Kraev",
-//          "username":"gelugu",
-//          "type":"private"
-//        },
-//      "date":1659529520,
-//      "text":"\u0422\u0440\u0435\u0432\u043e\u0436\u043d\u043e"
-//    }
-//  },
-//    {"update_id":119782898,"message":{"message_id":449,"from":{"id":412120748,"is_bot":false,"first_name":"Zlata","last_name":"Ulitina","username":"takoysyakoy","language_code":"en"},"chat":{"id":412120748,"first_name":"Zlata","last_name":"Ulitina","username":"takoysyakoy","type":"private"},"date":1659530164,"text":"/start",}},
-//    {"update_id":119782899,"message":{"message_id":450,"from":{"id":412120748,"is_bot":false,"first_name":"Zlata","last_name":"Ulitina","username":"takoysyakoy","language_code":"en"},"chat":{"id":412120748,"first_name":"Zlata","last_name":"Ulitina","username":"takoysyakoy","type":"private"},"date":1659530178,"text":"/start","entities":[{"offset":0,"length":6,"type":"bot_command"}]}}
-//  ]
-//}
