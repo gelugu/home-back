383b1f072c1686c7534a519e695c6eee53cbd402
gelugu <kraev.mixail@gmail.com>
2022-10-30 00:17:52 +0400

    single source version

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
-container_name=gelugu/home-back:1.0.0
-terraform_dir=cicd/terraform
-
-prepare:
-	echo todo
+version=1.0.0
+container_name=gelugu/home-back:${version}
 
 clean-db:
 	rm -r .db-data/postgres || echo "No db data found"
 db: clean-db
-	docker-compose up home-postgres pgadmin-home
+	docker-compose up
+
+build:
+	./gradlew shadowJar
 
 docker-build:
 	docker build -t ${container_name} -f cicd/Dockerfile .
@@ -15,15 +15,3 @@ docker-dev:
 	docker run -p 8080:8080 -e "DB_URL=localhost" -e "DB_NAME=home-postgres-db" -e "DB_PORT=5432" -e "DB_USER=home-postgres-user" -e "DB_PASSWORD=pass" ${container_name}
 docker-push:
 	docker push ${container_name}
-
-terraform-init:
-	terraform -chdir=${terraform_dir} init
-
-plan:
-	terraform -chdir=${terraform_dir} plan
-
-apply:
-	terraform -chdir=${terraform_dir} apply
-
-destroy:
-	terraform -chdir=${terraform_dir} destroy
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,43 +5,46 @@ val exposedVersion: String by project
 val postgresqlVersion: String by project
 
 plugins {
-    application
-    kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+  application
+  kotlin("jvm") version "1.7.10"
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+  id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.gelugu.home"
-version = "1.0.0"
 application {
-    mainClass.set("com.gelugu.home.ApplicationKt")
+  mainClass.set("com.gelugu.home.ApplicationKt")
 
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+  val isDevelopment: Boolean = project.ext.has("development")
+  applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+  archiveFileName.set("${project.name}.jar")
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-    implementation("io.ktor:ktor-server-cio-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-cors:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
+  implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+  implementation("io.ktor:ktor-server-cio-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-cors:$ktorVersion")
+  implementation("io.ktor:ktor-server-auth:$ktorVersion")
+  implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
 
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+  implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
-    // database
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
-    implementation("org.postgresql:postgresql:$postgresqlVersion")
+  // database
+  implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+  implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+  implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+  implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+  implementation("org.postgresql:postgresql:$postgresqlVersion")
 
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+  testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
